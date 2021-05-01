99ed1a0570cabebb7cb1e4caf0557560d0ae11cc
goodtrailer <aldenwu0@gmail.com>
2021-05-01 02:24:16 -0700

    Visual Studio code cleanup

--- DailyDesktop.Core.Providers.Unsplash/UnsplashProvider.cs
+++ DailyDesktop.Core.Providers.Unsplash/UnsplashProvider.cs
@@ -1,4 +1,6 @@
-﻿
+﻿// Copyright (c) Alden Wu <aldenwu0@gmail.com>. Licensed under the MIT Licence.
+// See the LICENSE file in the repository root for full licence text.
+
 using System.Net;
 using System.Text.RegularExpressions;
 
--- DailyDesktop.Core.Providers.WikimediaCommons/WikimediaCommonsProvider.cs
+++ DailyDesktop.Core.Providers.WikimediaCommons/WikimediaCommonsProvider.cs
@@ -1,5 +1,6 @@
-﻿using System;
-using System.IO;
+﻿// Copyright (c) Alden Wu <aldenwu0@gmail.com>. Licensed under the MIT Licence.
+// See the LICENSE file in the repository root for full licence text.
+
 using System.Net;
 using System.Text.RegularExpressions;
 
