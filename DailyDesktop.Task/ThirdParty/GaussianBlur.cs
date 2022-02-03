8cc10c9bbd13ea7fcc976de57a32d89a023efa09
goodtrailer <aldenwu0@gmail.com>
2022-02-03 03:11:04 -0800

    Meta: Fix third-party license headers

--- DailyDesktop.Task/ThirdParty/GaussianBlur.cs
+++ DailyDesktop.Task/ThirdParty/GaussianBlur.cs
@@ -1,5 +1,5 @@
-﻿// Copyright (c) Alden Wu <aldenwu0@gmail.com>. Licensed under the MIT Licence.
-// See the LICENSE file in the repository root for full licence text.
+﻿// Copyright (c) Michal Dymel <michal@dymel.pl>. Licensed under the MIT Licence.
+// See the EULA file in the repository root for full licence text.
 
 using System;
 using System.Collections.Generic;
