0d9263fde76c7d9d14b429ae1251b60552b47038
Alden Wu <aldenwu0@gmail.com>
2023-03-20 02:27:42 -0700

    Tests: Remove unnecessary using directive

--- DailyDesktop.Tests/TestProviders.cs
+++ DailyDesktop.Tests/TestProviders.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Alden Wu <aldenwu0@gmail.com>. Licensed under the MIT Licence.
 // See the LICENSE file in the repository root for full licence text.
 
-using System;
 using System.Threading.Tasks;
 using DailyDesktop.Core.Providers;
 using DailyDesktop.Providers.Bing;
